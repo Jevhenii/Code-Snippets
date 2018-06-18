883b62bbde96e3a52366599c3eb0b7d8d25bd64e
Yevhenii Podkorytov <yevheniipodkorytov@gmail.com>
2018-06-18 15:21:48 +0300

    Added styles and home page module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomePageModule } from './home-page/home-page.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomePageModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
