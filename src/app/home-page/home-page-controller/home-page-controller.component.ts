66017ace7524d89c15f39f62abbf357cd2affd07
Yevhenii Podkorytov <yevheniipodkorytov@gmail.com>
2018-07-04 14:28:48 +0300

    Make changes

--- src/app/home-page/home-page-controller/home-page-controller.component.ts
+++ src/app/home-page/home-page-controller/home-page-controller.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Snippet} from '../models/snippet';
+import {Languages} from '../models/temp.lenguages.enum';
 
 @Component({
   selector: 'app-home-page-controller',
@@ -6,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page-controller.component.scss']
 })
 export class HomePageControllerComponent implements OnInit {
-
+  snippets = [
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;'),
+    new Snippet(
+      'My snipshake',
+      Languages.typescript,
+      'Bring all boys to the yard',
+      'let betterThenYours = true;')
+  ]
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/home-page/models/temp.lenguages.enum.ts
@@ -0,0 +1,4 @@
+export enum languages {
+  typescript = 'TypeScript',
+  css = 'CSS'
+}
